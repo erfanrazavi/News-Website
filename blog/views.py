f86097d2da4e648c6a624b46537e294a223a79cc
Erfan Razavi <erfan6235@gmail.com>
2024-09-05 00:35:06 +0330

    pagination
    add paginator

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render , get_object_or_404
 from blog.models import Post ,Category
+from django.core.paginator import EmptyPage, Paginator , PageNotAnInteger
 
 # Create your views here.
 def blog_home_views(request,**kwargs):
     posts = Post.objects.filter(status=1)
-    
+   
     if kwargs.get('cat_name') != None:
         posts = posts.filter(category__name=kwargs['cat_name'])
+        
+    posts = Paginator(posts , 5)
+   
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except EmptyPage:
+            posts = posts.get_page(1)
+    except PageNotAnInteger:
+            posts = posts.get_page(1)
+
+
+    context = {'posts' : posts}
     
-    context = {'posts' : posts }
     return render(request , 'blog/post-list.html' , context)
 
 def blog_single_views(request ,**kwargs):
+    
     if 'pid' in kwargs:
         
         posts = get_object_or_404(Post, id=kwargs['pid'])
@@ -22,6 +36,7 @@ def blog_single_views(request ,**kwargs):
             posts = posts.filter(tags__name__in=[kwargs['tag_name']])
         context = {'posts': posts}
         return render(request, 'blog/post-list.html', context)
+    
     posts.counted_views += 1
     posts.save()
     context = {'posts' : posts }
